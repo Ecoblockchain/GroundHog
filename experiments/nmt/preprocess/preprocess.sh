8679888340caf18ae43cad08a214712d452e5b55
Darafei A. Praliaskouski <me@komzpa.net>
2016-07-17 16:00:42 +0300

    ngrams

--- experiments/nmt/preprocess/preprocess.sh
+++ experiments/nmt/preprocess/preprocess.sh
@@ -1,8 +1,9 @@
-python preprocess.py -p -o -c -t -d ../$1$2/vocab.$1.pkl -b ../$1$2/binarized.$1.pkl ../$1$2/from.txt
+#!/usr/bin/env bash
+python preprocess.py -p -o -c -t -n 3 -d ../$1$2/vocab.$1.pkl -b ../$1$2/binarized.$1.pkl ../$1$2/from.txt
 python invert-dict.py ../$1$2/vocab.$1.pkl ../$1$2/invocab.$1.pkl
 python convert-pkl2hdf5.py ../$1$2/binarized.$1.pkl ../$1$2/binarized.$1.h5
 
-python preprocess.py -p -o -c -t -d ../$1$2/vocab.$2.pkl -b ../$1$2/binarized.$2.pkl ../$1$2/to.txt
+python preprocess.py -p -o -c -t -n 3 -d ../$1$2/vocab.$2.pkl -b ../$1$2/binarized.$2.pkl ../$1$2/to.txt
 python invert-dict.py ../$1$2/vocab.$2.pkl ../$1$2/invocab.$2.pkl
 python convert-pkl2hdf5.py ../$1$2/binarized.$2.pkl ../$1$2/binarized.$2.h5
 
